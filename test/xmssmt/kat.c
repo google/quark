997efcc7ce4108e825e41edd781fe8e9bad24ea2
Dave Mercer <dmercer@google.com>
2018-07-05 23:13:33 -0700

    Fixing Subfunction tests

--- test/xmssmt/kat.c
+++ test/xmssmt/kat.c
@@ -23,6 +23,7 @@
 #include <test_vector.h>
 #include "verify.h"
 #include "macros.h"
+#include "quark_misc.h"
 
 /* List of header files containing XMSS reference vectors to be tested, please ensure    */
 /* that one and only one of these is uncommented                                         */
@@ -43,10 +44,9 @@
 #include "xmssmt_ref_60_12.h"
 #include "xmssmt_ref_001.h"
 
-/* Define the below value if you want to test out sub-functions but make */
-/* sure you only use xmss_ref_001.h and xmssmt_ref_001.h vector sets in  */
-/* this case.                                                            */
-#undef _QUARK_TEST_REF_001_H_
+/** Define this to test subfunctions **/
+#define _QUARK_TEST_XMSS_MT_SUB_FUNCTIONS_
+void testSubfunctions();
 
 /* Implements a quick-and-dirty known-answer-test (KAT) command line utility */
 /* that allows you to check and see if your signature primitive is (still!)  */
@@ -55,8 +55,6 @@
 /* that it catches a single bit-flip on the message yielding an invalid      */
 /* signature.  Pretty basic stuff...                                         */
 int main(void) {
-  sig_retcode_t result;
-
   g_cacheStart = 0;
   g_cacheEnd = 0;
 
@@ -64,18 +62,12 @@ int main(void) {
   srand(time(NULL));
   {
     printf("XMSS Tests\n");
-#ifndef _QUARK_TEST_REF_001_H_
     const test_vector testVectors[] = {
         xmss_vector_001,
         xmss_vector_10,
         xmss_vector_16,
         xmss_vector_20,
     };
-#else
-    const test_vector testVectors[] = {
-        xmss_vector_001,
-    };
-#endif
 
     for (size_t v = 0; v < sizeof(testVectors) / sizeof(testVectors[0]); v++) {
       printf("-----Using Vector: %s-----\n", testVectors[v].name);
@@ -83,211 +75,6 @@ int main(void) {
       uint8_t msg_corrupt[testVectors[v].msgLen];
       memcpy(msg_corrupt, testVectors[v].msg, testVectors[v].msgLen);
       msg_corrupt[8] ^= 0x1;
-
-#ifdef _QUARK_TEST_REF_001_H_
-      /* Test ltreeSmall( ) vs ltreeBig( ) */
-      if ( 0 )
-      {
-          uint32_t a0[ADRS_WORD_COUNT] = { 0 };
-          uint32_t a1[ADRS_WORD_COUNT] = { 0 };
-          uint8_t *pk0, *pk1;
-          uint8_t seed[MAX_HASH_LEN], res0[MAX_HASH_LEN], res1[MAX_HASH_LEN];
-          xmssmt_params_t params;
-          int passCnt = 0;
-          int failCnt = 0;
-          int testCnt = 1000;
-
-          /* Initialize params */
-          parseXmssOid( &params, XMSS_SHA2_10_256 );
-
-          /* Allocate public key arrays */
-          pk0 = malloc( sizeof(uint8_t) * params.otsSigLen );
-          pk1 = malloc( sizeof(uint8_t) * params.otsSigLen );
-          if ( ( pk0 == NULL ) || ( pk1 == NULL ) )
-          {
-              free(pk0);
-              free(pk1);
-              printf( "[ERROR] Unable to allocate public key arrays\n" );
-              return;
-          }
-
-          for ( int k = 0; k < testCnt; k++ )
-          {
-              /* Initialize seed */
-              for ( int i = 0; i < MAX_HASH_LEN; i++ )
-                  seed[i] = rand( ) & 0xff;
-
-              /* Initialize PK0 and PK1 */
-              for ( int i = 0; i < params.otsSigLen; i++ )
-                  pk0[i] = rand( ) & 0xff;
-              memcpy( pk1, pk0, params.otsSigLen );
-
-              /* Compute ltree */
-              ltreeBig( res0, pk0, seed, a0, &params );
-              ltreeSmall( res1, pk1, seed, a1, &params );
-
-              /* Compare results and update counters */
-              if ( !memcmp( res0, res1, params.n ) )
-                  passCnt++;
-              else
-                  failCnt++;
-          }
-
-          printf( "[RESULTS] Executed %d tests\n", testCnt );
-          printf( "    PASS = %d\n", passCnt );
-          printf( "    FAIL = %d\n", failCnt );
-      }
-
-      /* Test computeHmsg */
-      if ( 0 )
-      {
-          uint32_t adrs[ADRS_WORD_COUNT] = { 0 };
-          uint8_t messageHash[MAX_HASH_LEN];
-          xmssmt_params_t params;
-          uint64_t idx;
-
-          parseXmssOid( &params, XMSS_SHA2_10_256 );
-          idx = readBigEndian( sig, params.idxLen );
-          printf( "-------------- Testing Compute Message Hash --------------\n" );
-          result = computeHmsg(messageHash,
-                               sig + params.idxLen,
-                               pk + XMSS_PUB_ROOT_OFFSET,
-                               idx,
-                               msg,
-                               2290,
-                               &params);
-          if ( !memcmp( messageHash, mhash, params.n ) && result )
-              printf( "[RESULTS] message hash computation: PASSED\n" );
-          else
-              printf( "[RESULTS] message hash computation: FAILED\n" );
-      }
-
-      /* Test otsPkFromSig( ) */
-      if ( 0 )
-      {
-          uint32_t adrs[ADRS_WORD_COUNT] = { 0 };
-          uint8_t *computedPk;
-          xmssmt_params_t params;
-
-          adrs[ADRS_OTS_ADDR] = 257;
-          parseXmssOid( &params, XMSS_SHA2_10_256 );
-          computedPk = malloc( sizeof(uint8_t) * params.otsSigLen );
-          if ( computedPk == NULL )
-          {
-              free(computedPk);
-              printf( "[ERROR] Unable to allocate public key array\n" );
-              return;
-          }
-
-          printf( "-------------- Testing WOTS Pk Computation --------------\n" );
-          result = otsPkFromSig( computedPk,
-                                 mhash,
-                                 sig + XMSS_SIG_OTS_SIG_OFFSET( params.n ),
-                                 pk + XMSS_PUB_SEED_OFFSET( params.n ),
-                                 adrs,
-                                 &params);
-
-          if ( !memcmp( computedPk, wots_pk, params.otsSigLen ) && result )
-              printf( "[RESULTS] WOTS+ Pk computation: PASSED\n" );
-          else
-              printf( "[RESULTS] WOTS+ Pk computation: FAILED\n" );
-      }
-
-      /* Test ltree( ) */
-      if ( 0 )
-      {
-          uint32_t adrs[ADRS_WORD_COUNT] = { 0 };
-          uint8_t computedLeaf[MAX_HASH_LEN];
-          xmssmt_params_t params;
-
-          adrs[ADRS_OTS_ADDR] = 257;
-          adrs[ADRS_TYPE]     = ADRS_TYPE_LTREE;
-          parseXmssOid( &params, XMSS_SHA2_10_256 );
-
-          printf( "-------------- Testing ltreeSmall( ) Computation --------------\n" );
-          result = ltreeSmall( computedLeaf,
-                               wots_pk,
-                               pk + XMSS_PUB_SEED_OFFSET( params.n ),
-                               adrs,
-                               &params );
-          if ( !memcmp( computedLeaf, leaf, params.n ) && result )
-              printf( "[RESULTS] ltreeSmall computation: PASSED\n" );
-          else
-              printf( "[RESULTS] ltreeSmall computation: FAILED\n" );
-      }
-
-      /* Test computeRoot( ) */
-      if ( 0 )
-      {
-          uint32_t adrs[ADRS_WORD_COUNT] = { 0 };
-          uint8_t computedRoot[MAX_HASH_LEN];
-          xmssmt_params_t params;
-          uint64_t idx;
-
-          adrs[ADRS_TYPE]     = ADRS_TYPE_LEAF;
-          parseXmssOid( &params, XMSS_SHA2_10_256 );
-          idx = readBigEndian( sig, params.idxLen );
-          printf( "-------------- Testing computeRoot( ) Computation --------------\n" );
-          result = computeRoot( computedRoot,
-                                leaf,
-                                sig + XMSS_SIG_AUTH_OFFSET( params.n, params.p ),
-                                pk + XMSS_PUB_SEED_OFFSET( params.n ),
-                                adrs,
-                                idx,
-                                &params );
-          if ( !memcmp( computedRoot, root, params.n ) && result )
-              printf( "[RESULTS] computeRoot computation: PASSED\n" );
-          else
-              printf( "[RESULTS] computeRoot computation: FAILED\n" );
-      }
-
-      /* Test new incremental leaf-generation routine against original approach */
-      if ( 0 )
-      {
-          uint32_t adrs[ADRS_WORD_COUNT] = { 0 };
-          uint32_t adrsOts[ADRS_WORD_COUNT] = { 0 };
-          uint32_t adrsLtree[ADRS_WORD_COUNT] = { 0 };
-          xmssmt_params_t params;
-
-          parseXmssOid( &params, XMSS_SHA2_10_256 );
-
-          /* Compute leaf value using original method */
-          uint8_t oldPk[params.otsSigLen];
-          uint8_t oldLeaf[params.n], newLeaf[params.n];
-          uint8_t *SEED = pk + XMSS_PUB_SEED_OFFSET( params.n );
-          adrs[ADRS_OTS_ADDR] = 257;
-          printf( "-------------- Testing computeLeaf( ) --------------\n" );
-          otsPkFromSig( oldPk,
-                        mhash,
-                        sig + XMSS_SIG_OTS_SIG_OFFSET( params.n ),
-                        SEED,
-                        adrs,
-                        &params );
-          adrs[ADRS_OTS_ADDR] = 257;
-          adrs[ADRS_TYPE]     = ADRS_TYPE_LTREE;
-          ltreeSmall( oldLeaf,
-                      oldPk,
-                      SEED,
-                      adrs,
-                      &params );
-
-          adrsOts[ADRS_TYPE]       = ADRS_TYPE_OTS;
-          adrsOts[ADRS_OTS_ADDR]   = 257;
-          adrsLtree[ADRS_TYPE]     = ADRS_TYPE_LTREE;
-          adrsLtree[ADRS_OTS_ADDR] = 257;
-          computeLeaf( newLeaf,
-                       mhash,
-                       sig + XMSS_SIG_OTS_SIG_OFFSET( params.n ),
-                       SEED,
-                       adrsOts,
-                       adrsLtree,
-                       &params );
-          if ( !memcmp( oldLeaf, newLeaf, params.n ) )
-              printf( "[RESULTS] computeLeaf computation: PASSED\n" );
-          else
-              printf( "[RESULTS] computeLeaf computation: FAILED\n" );
-      }
-#endif  /* _QUARK_TEST_REF_001_H_ */
 
       /* Putting it all together to test xmssVerifySignature( ) */
       printf("Testing xmssVerifySignature");
@@ -360,8 +147,8 @@ int main(void) {
     };
 #else
     const test_vector testVectors[] = {
-      mt_vector_001,
-  };
+        mt_vector_001,
+    };
 #endif
     for (size_t v = 0; v < sizeof(testVectors) / sizeof(testVectors[0]); v++) {
       printf("-----Using Vector: %s-----\n", testVectors[v].name);
@@ -387,7 +174,7 @@ int main(void) {
                                                                            testVectors[v].sigLen,
                                                                            testVectors[v].key,
                                                                            testVectors[v].keyLen));
-IF_PASS_MSG
+      IF_PASS_MSG
       /* Putting it all together to test xmssMtVerifySignatureFlash( ) */
       uint32_t signatureOffset = 0;
       size_t scratchLen = QUARK_SCRATCH_SIZE;
@@ -421,11 +208,199 @@ IF_PASS_MSG
     }
   }
 
-  if(g_numberOfTestFailures != 0){
+  testSubfunctions();
+
+  if (g_numberOfTestFailures != 0) {
     printf("\n%d tests FAILED\n", g_numberOfTestFailures);
     return 1;
   }
 
   printf("\nAll tests PASSED\n");
   return 0;
+}
+
+void testSubfunctions() {
+#ifdef _QUARK_TEST_XMSS_MT_SUB_FUNCTIONS_
+  int result = 0;
+  /* Test ltreeSmall( ) vs ltreeBig( ) */
+  uint32_t a0[ADRS_WORD_COUNT] = {0};
+  uint32_t a1[ADRS_WORD_COUNT] = {0};
+  uint8_t *pk0, *pk1;
+  uint8_t seed[MAX_HASH_LEN], res0[MAX_HASH_LEN], res1[MAX_HASH_LEN];
+  xmssmt_params_t params;
+  int passCnt = 0;
+  int failCnt = 0;
+  int testCnt = 1000;
+
+  /* Initialize params */
+  parseXmssOid(&params, XMSS_SHA2_10_256);
+
+  /* Allocate public key arrays */
+  pk0 = malloc(sizeof(uint8_t) * params.otsSigLen);
+  pk1 = malloc(sizeof(uint8_t) * params.otsSigLen);
+  if ((pk0 == NULL) || (pk1 == NULL)) {
+    free(pk0);
+    free(pk1);
+    printf("[ERROR] Unable to allocate public key arrays\n");
+    return;
+  }
+
+  for (int k = 0; k < testCnt; k++) {
+    /* Initialize seed */
+    for (int i = 0; i < MAX_HASH_LEN; i++)
+      seed[i] = rand() & 0xff;
+
+    /* Initialize PK0 and PK1 */
+    for (size_t i = 0; i < params.otsSigLen; i++)
+      pk0[i] = rand() & 0xff;
+    memcpy(pk1, pk0, params.otsSigLen);
+
+    /* Compute ltree */
+    ltreeBig(res0, pk0, seed, a0, &params);
+    ltreeSmall(res1, pk1, seed, a1, &params);
+
+    /* Compare results and update counters */
+    if (!memcmp(res0, res1, params.n))
+      passCnt++;
+    else
+      failCnt++;
+  }
+
+  printf("[RESULTS] Executed %d tests\n", testCnt);
+  printf("    PASS = %d\n", passCnt);
+  printf("    FAIL = %d\n", failCnt);
+
+
+    /* Test computeHmsg */
+    uint32_t adrs[ADRS_WORD_COUNT] = {0};
+    uint8_t messageHash[MAX_HASH_LEN];
+    uint64_t idx;
+
+    parseXmssOid(&params, XMSS_SHA2_10_256);
+    idx = readBigEndian(sig_001, params.idxLen);
+    printf("-------------- Testing Compute Message Hash --------------\n");
+    result = computeHmsg(messageHash,
+                         sig_001 + params.idxLen,
+                         pk_001 + XMSS_PUB_ROOT_OFFSET,
+                         idx,
+                         msg_001,
+                         2290,
+                         &params);
+    if (!memcmp(messageHash, mhash_001, params.n) && result)
+      printf("[RESULTS] message hash computation: PASSED\n");
+    else
+      printf("[RESULTS] message hash computation: FAILED\n");
+
+
+    /* Test otsPkFromSig( ) */
+    memset(adrs, 0, sizeof(adrs));
+    uint8_t *computedPk;
+
+    adrs[ADRS_OTS_ADDR] = 257;
+    parseXmssOid(&params, XMSS_SHA2_10_256);
+    computedPk = malloc(sizeof(uint8_t) * params.otsSigLen);
+    if (computedPk == NULL) {
+      free(computedPk);
+      printf("[ERROR] Unable to allocate public key array\n");
+      return;
+    }
+
+    printf("-------------- Testing WOTS Pk Computation --------------\n");
+    result = otsPkFromSig(computedPk,
+                          mhash_001,
+                          sig_001 + XMSS_SIG_OTS_SIG_OFFSET(params.n),
+                          pk_001 + XMSS_PUB_SEED_OFFSET(params.n),
+                          adrs,
+                          &params);
+
+    if (!memcmp(computedPk, wots_pk_001, params.otsSigLen) && result)
+      printf("[RESULTS] WOTS+ Pk computation: PASSED\n");
+    else
+      printf("[RESULTS] WOTS+ Pk computation: FAILED\n");
+
+    /* Test ltree( ) */
+    memset(adrs, 0, sizeof(adrs));
+    uint8_t computedLeaf[MAX_HASH_LEN];
+
+    adrs[ADRS_OTS_ADDR] = 257;
+    adrs[ADRS_TYPE] = ADRS_TYPE_LTREE;
+    parseXmssOid(&params, XMSS_SHA2_10_256);
+
+    printf("-------------- Testing ltreeSmall( ) Computation --------------\n");
+    result = ltreeSmall(computedLeaf,
+                        wots_pk_001,
+                        pk_001 + XMSS_PUB_SEED_OFFSET(params.n),
+                        adrs,
+                        &params);
+    if (!memcmp(computedLeaf, leaf_001, params.n) && result)
+      printf("[RESULTS] ltreeSmall computation: PASSED\n");
+    else
+      printf("[RESULTS] ltreeSmall computation: FAILED\n");
+
+
+    /* Test computeRoot( ) */
+    memset(adrs, 0, sizeof(adrs));
+    uint8_t computedRoot[MAX_HASH_LEN];
+
+    adrs[ADRS_TYPE] = ADRS_TYPE_LEAF;
+    parseXmssOid(&params, XMSS_SHA2_10_256);
+    idx = readBigEndian(sig_001, params.idxLen);
+    printf("-------------- Testing computeRoot( ) Computation --------------\n");
+    result = computeRoot(computedRoot,
+                         leaf_001,
+                         sig_001 + XMSS_SIG_AUTH_OFFSET(params.n, params.p),
+                         pk_001 + XMSS_PUB_SEED_OFFSET(params.n),
+                         adrs,
+                         idx,
+                         &params);
+    if (!memcmp(computedRoot, root_001, params.n) && result)
+      printf("[RESULTS] computeRoot computation: PASSED\n");
+    else
+      printf("[RESULTS] computeRoot computation: FAILED\n");
+
+
+    /* Test new incremental leaf-generation routine against original approach */
+    memset(adrs, 0, sizeof(adrs));
+    uint32_t adrsOts[ADRS_WORD_COUNT] = {0};
+    uint32_t adrsLtree[ADRS_WORD_COUNT] = {0};
+
+    parseXmssOid(&params, XMSS_SHA2_10_256);
+
+    /* Compute leaf value using original method */
+    uint8_t oldPk[params.otsSigLen];
+    uint8_t oldLeaf[params.n], newLeaf[params.n];
+    uint8_t SEED[sizeof(pk_001) - XMSS_PUB_SEED_OFFSET(params.n)];
+    memcpy(SEED, &pk_001[XMSS_PUB_SEED_OFFSET(params.n)], sizeof(SEED));
+    adrs[ADRS_OTS_ADDR] = 257;
+    printf("-------------- Testing computeLeaf( ) --------------\n");
+    otsPkFromSig(oldPk,
+                 mhash_001,
+                 sig_001 + XMSS_SIG_OTS_SIG_OFFSET(params.n),
+                 SEED,
+                 adrs,
+                 &params);
+    adrs[ADRS_OTS_ADDR] = 257;
+    adrs[ADRS_TYPE] = ADRS_TYPE_LTREE;
+    ltreeSmall(oldLeaf,
+               oldPk,
+               SEED,
+               adrs,
+               &params);
+
+    adrsOts[ADRS_TYPE] = ADRS_TYPE_OTS;
+    adrsOts[ADRS_OTS_ADDR] = 257;
+    adrsLtree[ADRS_TYPE] = ADRS_TYPE_LTREE;
+    adrsLtree[ADRS_OTS_ADDR] = 257;
+    computeLeaf(newLeaf,
+                mhash_001,
+                sig_001 + XMSS_SIG_OTS_SIG_OFFSET(params.n),
+                SEED,
+                adrsOts,
+                adrsLtree,
+                &params);
+    if (!memcmp(oldLeaf, newLeaf, params.n))
+      printf("[RESULTS] computeLeaf computation: PASSED\n");
+    else
+      printf("[RESULTS] computeLeaf computation: FAILED\n");
+#endif  /* _QUARK_TEST_XMSS_MT_SUB_FUNCTIONS_ */
 }
